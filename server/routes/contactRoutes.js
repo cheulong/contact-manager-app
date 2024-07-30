cf3b5a01aeb731d5d2c1adff1261ec762763deed
Cheulong Sear <searcheulong@gmail.com>
2024-07-30 14:49:20 +0700

    create router for crud

---
+++ server/routes/contactRoutes.js
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.route('/').get((req, res) => {
+  res.status(200).json({
+    message: 'Get all contacts'
+  });
+}).post((req, res) => {
+  res.status(201).json({
+    message: 'Create new contact'
+  });
+});
+
+router.route('/:id').get((req, res) => {
+  res.status(200).json({
+    message: `Get contact for ${req.params.id}`
+  });
+}).put((req, res) => {
+  res.status(200).json({
+    message: `Update contact for ${req.params.id}`
+  });
+}).delete((req, res) => {
+  res.status(200).json({
+    message: `Delete contact for ${req.params.id}`
+  });
+});
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import contactRoutes from './routes/contactRoutes.js';
+
+dotenv.config();
+
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use('/api/contacts', contactRoutes)
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
